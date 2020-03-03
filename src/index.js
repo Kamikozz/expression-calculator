f13f3c6eb56461ab5abf4b461bdc86dd115ce1d6
Alexey Shevchenko <kamikozz@mail.ru>
2020-03-03 21:30:09 +0300

    refactor: change consts, rename vars, add comments

--- src/index.js
+++ src/index.js
@@ -12,67 +12,52 @@ function parser(arr) {
             case '-':
             case '*':
             case '/':
+                // if we reached an operation -> add item before & operation
                 parsed.push(Number(item), arr[i]);
                 item = '';
                 break;
             default: item += arr[i]; break;
         }
     }
+    // if it's the end of the line -> push item before
     if (item.length) parsed.push(Number(item));
     return parsed;
 }
 
+/**
+ * Evaluate the given expression (WITHOUT BRACKETS)
+ * @param {Array} arr unparsed array of the numbers & operations
+ * @returns {number} result of the evaluated expression
+ */
 function eval(arr) {
-    let parsed = parser(arr);
-
     const OPERATIONS = ['/', '*', '-', '+'];
+    let parsed = parser(arr); // get parsed array with numbers & operations
     while (parsed.length !== 1) {
-        let currentOperation = 0;
+        for (let i = 0; i < OPERATIONS.length; i++) {
+            const OPERATION = OPERATIONS[i];
 
-        // Division
-        let posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        while (posOperation !== -1) {
-            // console.log(`--${OPERATIONS[currentOperation]}--`);
-            let tempResult;
-            if (parsed[posOperation + 1] === 0)
-                throw new Error('TypeError: Division by zero.');
-            tempResult = parsed[posOperation - 1] / parsed[posOperation + 1];
-            // console.log(tempResult);
-            parsed.splice(posOperation - 1, 3, tempResult);
-            posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        }
-        currentOperation++;
-
-        posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        while (posOperation !== -1) {
-            // console.log(`--${OPERATIONS[currentOperation]}--`);
-            let tempResult;
-            tempResult = parsed[posOperation - 1] * parsed[posOperation + 1];
-            // console.log(tempResult);
-            parsed.splice(posOperation - 1, 3, tempResult);
-            posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        }
-        currentOperation++;
-
-        posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        while (posOperation !== -1) {
-            // console.log(`--${OPERATIONS[currentOperation]}--`);
-            let tempResult;
-            tempResult = parsed[posOperation - 1] - parsed[posOperation + 1];
-            // console.log(tempResult);
-            parsed.splice(posOperation - 1, 3, tempResult);
-            posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        }
-        currentOperation++;
-
-        posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
-        while (posOperation !== -1) {
-            // console.log(`--${OPERATIONS[currentOperation]}--`);
-            let tempResult;
-            tempResult = parsed[posOperation - 1] + parsed[posOperation + 1];
-            // console.log(tempResult);
-            parsed.splice(posOperation - 1, 3, tempResult);
-            posOperation = parsed.indexOf(OPERATIONS[currentOperation]);
+            let posOperation = parsed.indexOf(OPERATION);
+            while (posOperation !== -1) {
+                let tempResult;
+                const operand1 = parsed[posOperation - 1];
+                const operand2 = parsed[posOperation + 1];
+                switch (OPERATION) {
+                    case '/':
+                        // if next item aka operand2 is equal to zero
+                        if (parsed[posOperation + 1] === 0)
+                            throw new Error('TypeError: Division by zero.');
+                              tempResult = operand1 / operand2; break;
+                    case '*': tempResult = operand1 * operand2; break;
+                    case '-': tempResult = operand1 - operand2; break;
+                    case '+': tempResult = operand1 + operand2; break;
+                }
+                // replace items to the left & to the right
+                // from the found position of an operation
+                // with item of an evaluated result
+                // 3 items: operand1, operation, operand2
+                parsed.splice(posOperation - 1, 3, tempResult);
+                posOperation = parsed.indexOf(OPERATION);
+            }
         }
     }
     return parsed[0];
@@ -93,14 +78,25 @@ function expressionCalculator(expr) {
     if (bracketsCount !== 0)
         throw new Error('ExpressionError: Brackets must be paired');
 
+    // get rid of the brackets (if they are present)
+    // find the most nested brackets
     let start = expr.lastIndexOf('(');
-    // there are brackets somewhere
     while (start !== -1) {
+        // find the closing bracket
         let end = expr.indexOf(')') + 1;
+        // be sure, we found the correct closing bracket
         while (end < start) end = expr.indexOf(')', end) + 1;
 
-        let cutArr = expr.slice(start + 1, end - 1);
-        expr.splice(start, end - start, eval(cutArr));
+        // cut found bracket expression from the source expression
+        // (WITHOUT BRACKETS)
+        let cutArray = expr.slice(start + 1, end - 1);
+
+        // replace the most nested bracket expression (WITH BRACKETS)
+        // in the source expression
+        // with evaluated most nested bracket expression
+        expr.splice(start, end - start, eval(cutArray));
+
+        // find next most nested brackets
         start = expr.lastIndexOf('(');
     }
 
@@ -111,13 +107,3 @@ function expressionCalculator(expr) {
 module.exports = {
     expressionCalculator
 }
-
-// console.log(module.exports.expressionCalculator('2 + 2'));
-// console.log(module.exports.expressionCalculator('2-2'));
-// console.log(module.exports.expressionCalculator('2*3'));
-// console.log(module.exports.expressionCalculator('1/2'));
-// console.log(module.exports.expressionCalculator('1 / 0'));
-// console.log(module.exports.expressionCalculator(' (49 * 63) / (58 * (3 * 36)) '));
-// console.log(module.exports.expressionCalculator('88 - 72 + 55 * 57'));
-// console.log(module.exports.expressionCalculator('48 + 59 * 86 * 92 * 23'));
-// console.log(module.exports.expressionCalculator(' 49 * 63 / 58 * 36  '));
